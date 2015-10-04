bf295d70f706fe56d4583a464ecec1a95fda2ae0
Andres Matte <matte.andres@gmail.com>
2015-10-04 17:17:57 -0300

    ahora recibe archivo argumento

--- Assembler/Assembler/Program.cs
+++ Assembler/Assembler/Program.cs
@@ -9,8 +9,8 @@ namespace Assembler
         static void Main(string[] args)
         {
             AssemblyInterpreter interpreter = new AssemblyInterpreter();
-            //string text = File.ReadAllText(args[0]);
-            string text = File.ReadAllText("./Ejemplo7.txt");
+            string text = File.ReadAllText(args[0]);
+            //string text = File.ReadAllText("./Ejemplo6.txt");
             interpreter.Interpret(text);
             string output = DocumentBuilder.GenerateFinalDocument(interpreter.GetInstructions());
             File.WriteAllText(@"./output.txt", output);
